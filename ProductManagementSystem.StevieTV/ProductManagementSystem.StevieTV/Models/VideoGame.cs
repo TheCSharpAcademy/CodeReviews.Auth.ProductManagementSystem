91f18ea00f2d6720875c4bf2cc2b7f962e9cb29a
Stevie Robinson <stevie.robinson@gmail.com>
2024-04-11 20:42:51 +0200

    Annotate Model

--- ProductManagementSystem.StevieTV/ProductManagementSystem.StevieTV/Models/VideoGame.cs
+++ ProductManagementSystem.StevieTV/ProductManagementSystem.StevieTV/Models/VideoGame.cs
@@ -1,10 +1,19 @@
-﻿namespace ProductManagementSystem.StevieTV.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProductManagementSystem.StevieTV.Models;
 
 public class VideoGame
 {
     public int Id { get; set; }
     public string Name { get; set; }
+    
+    [Display(Name = "Active?")]
     public bool IsActive { get; set; }
+    [DataType(DataType.Date)]
+    [Display(Name = "Date Added")]
     public DateOnly DateAdded { get; set; }
+    [DataType(DataType.Currency)]
+    [Precision(10,2)]
     public decimal Price { get; set; }
 }
