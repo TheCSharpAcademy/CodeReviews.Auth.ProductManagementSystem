09451dbee4782bb3bb777844cd7e9f4a6f017f35
SERDAR SİPAHİOĞLU <94632226+Doc415@users.noreply.github.com>
2024-06-12 17:03:51 +0300

    tartup

--- ProductManagementSystem.StevieTV/ProductManagementSystem.StevieTV/Models/ErrorLog.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ProductManagementSystem.StevieTV.Models;
-
-public class ErrorLog
-{
-    public int Id { get; set; }
-    public string Error { get; set; }
-    public DateTime Time { get; set; }
-}
--- ProductManagementSystem.StevieTV/ProductManagementSystem.StevieTV/Models/UserRolesViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ProductManagementSystem.StevieTV.Models;
-
-public class UserRolesViewModel
-{
-    public string UserId { get; set; }
-    public string Email { get; set; }
-    public IEnumerable<string> Roles { get; set; }
-}
